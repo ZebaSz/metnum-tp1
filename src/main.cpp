2aa6cfbf63a2abc13fa8ea9c77578b2eba9fd9a0
Kevin Frachtenberg <kevinfra94@gmail.com>
2017-08-30 17:54:34 -0300

    Added tests for matrix norms

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "matrix.h"
+#include "gauss.h"
+
+using namespace std;
+
+int main() {
+    cout << "Ayuda! no estoy implementado y seba pide que me implementen" << endl;
+    return 0;
+}
--- test/MatrixTest.cpp
+++ test/MatrixTest.cpp
@@ -19,8 +19,26 @@ TEST(MatrixTest, square_array) {
             ASSERT_EQ(mx[i][j], value);
         }
     }
+
     for (int i = 0; i < size; ++i) {
         delete[] arr[i];
     }
     delete[] arr;
+}
+
+TEST(MatrixTest, norm){
+    int size = 5;
+    int value = 1;
+    int** arr = new int*[size];
+    for (int i = 0; i < size; ++i) {
+        arr[i] = new int[size];
+        for (int j = 0; j < size; ++j) {
+            arr[i][j] = value;
+        }
+    }
+    matrix<int> mx = toMatrix(arr, size, size);
+    ASSERT_EQ(infinityNorm(mx), 1);
+    ASSERT_EQ(twoNorm(mx), 5);
+    ASSERT_EQ(singleNorm(mx), 25);
+
 }
