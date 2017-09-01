e57a182674c8b7c5e2a2007df8d8db9512881798
Kevin Frachtenberg <kevinfra94@gmail.com>
2017-09-01 18:47:55 -0300

    Agrego test para chequear que cholesky no factoriza matrices no simetricas

--- test/CholeskyTest.cpp
+++ test/CholeskyTest.cpp
@@ -57,3 +57,13 @@ TEST(CholeskyTest, factorize4by4Matrix) {
     ASSERT_EQ(l[3][2], 1) << "i=" << 3 << ",j=" << 2;
     ASSERT_EQ(l[3][3], 1) << "i=" << 3 << ",j=" << 3;
 }
+
+TEST(CholeskyTest, cannotFactorizeNonSymmetricMatrix) {
+    matrix<float> m(4, row<float>(4, 0));
+    m[0][0] = 1; m[0][1] = 1; m[0][2] = 1; m[0][3] = 1;
+    m[1][0] = 1; m[1][1] = 2; m[1][2] = 2; m[1][3] = 2;
+    m[2][0] = 2; m[2][1] = 2; m[2][2] = 3; m[2][3] = 3;
+    m[3][0] = 6; m[3][1] = 2; m[3][2] = 3; m[3][3] = 4;
+
+    ASSERT_DEATH(cholesky_factorization(m), "Assertion");
+}
