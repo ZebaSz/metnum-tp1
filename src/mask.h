a7ec599c839f5daa6fea0fa58192c20fd9bd925c
manumena <manuelmena1993@gmail.com>
2017-09-04 02:29:55 -0300

    Add namespace to mask

--- src/mask.h
+++ src/mask.h
@@ -4,37 +4,39 @@
 #include <iostream>
 #include "matrix.h"
 
-struct circle {
-    circle(int x, int y, int r) : xCenter(x), yCenter(y), radius(r) {}
-    int xCenter;
-    int yCenter;
-    int radius;
-};
+using namespace std;
 
-template <typename T>
-circle get_center_and_radius(const matrix<T>& mask) {
-    int leftColumn = Matrix::columns(mask) - 1;
-    int rightColumn = 0;
-    int upperRow = Matrix::rows(mask) - 1;
-    int lowerRow = 0;
+namespace Mask {
+    struct circle {
+        circle(int x, int y, int r) : xCenter(x), yCenter(y), radius(r) {}
+        int xCenter;
+        int yCenter;
+        int radius;
+    };
 
-    for (int i = 0; i < Matrix::rows(mask); i++) {
-        for (int j = 0; j < Matrix::columns(mask); j++) {
-            if (mask[i][j] > 250) {
-                if (j < leftColumn) leftColumn = j;
-                if (j > rightColumn) rightColumn = j;
-                if (i < upperRow) upperRow = i;
-                if (i > lowerRow) lowerRow = i;
+    template <typename T>
+    circle get_center_and_radius(const matrix<T>& mask) {
+        int leftColumn = Matrix::columns(mask) - 1;
+        int rightColumn = 0;
+        int upperRow = Matrix::rows(mask) - 1;
+        int lowerRow = 0;
+
+        for (int i = 0; i < Matrix::rows(mask); i++) {
+            for (int j = 0; j < Matrix::columns(mask); j++) {
+                if (mask[i][j] > 250) {
+                    if (j < leftColumn) leftColumn = j;
+                    if (j > rightColumn) rightColumn = j;
+                    if (i < upperRow) upperRow = i;
+                    if (i > lowerRow) lowerRow = i;
             }
         }
+
+        int radius = (rightColumn - leftColumn + 1) / 2;
+        int xCenter = leftColumn + radius + 1;
+        int yCenter = upperRow + radius + 1;
+
+        return circle(xCenter, yCenter, radius);
     }
-
-    int radius = (rightColumn - leftColumn + 1) / 2;
-    int xCenter = leftColumn + radius + 1;
-    int yCenter = upperRow + radius + 1;
-
-    return circle(xCenter, yCenter, radius);
-}
 
 /**
  * Apply a mask to an image, returning a matrix adjusted to the mask size. The
@@ -62,24 +64,21 @@ matrix<T> apply_mask(const matrix<T>& img, const matrix<T>& mask) {
                 if (i > lowerRow) lowerRow = i;
             }
         }
-    }
 
-    std::cout << "U: " << upperRow << " Lo: " << lowerRow
-              << "Le: " << leftColumn << "R: " << rightColumn << std::endl;
-
-    int resultRows = lowerRow - upperRow + 1;
-    int resultColumns = rightColumn - leftColumn + 1;
-    matrix<T> result(resultRows, row<T>(resultColumns));
-    for (int i = 0; i < resultRows; i++) {
-        for (int j = 0; j < resultColumns; j++) {
-            if (mask[upperRow + i][leftColumn + j] > 250)
-                result[i][j] = img[upperRow + i][leftColumn + j];
-            else
-                result[i][j] = 0;
+        int resultRows = lowerRow - upperRow + 1;
+        int resultColumns = rightColumn - leftColumn + 1;
+        matrix<T> result(resultRows, row<T>(resultColumns));
+        for (int i = 0; i < resultRows; i++) {
+            for (int j = 0; j < resultColumns; j++) {
+                if (mask[upperRow + i][leftColumn + j] > 250)
+                    result[i][j] = img[upperRow + i][leftColumn + j];
+                else
+                    result[i][j] = 0;
+            }
         }
-    }
 
-    return result;
+        return result;
+    }
 }
 
 #endif //METNUM_TP1_MASK_H
