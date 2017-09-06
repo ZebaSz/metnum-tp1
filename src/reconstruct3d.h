93cf6dbfcfc410d99de8ac6e75ca3ab13ef4b15a
Ignacio Tarrio <itarrio@despegar.com>
2017-09-06 20:28:05 -0300

    wip depth

--- src/reconstruct3d.h
+++ src/reconstruct3d.h
@@ -6,6 +6,10 @@
 #include "gauss.h"
 #include "calibration.h"
 
+struct options {
+    options(bool maskOptimization) : maskOptimization(maskOptimization) {}
+    bool maskOptimization;
+};
 matrix<double> sourceOfLightMatrix(const direction &s1, const direction &s2, const direction &s3) {
     return {
             {s1.x, s2.x, s3.x},
@@ -23,18 +27,15 @@ matrix<double> sourceOfLightMatrix(const direction &s1, const direction &s2, con
  *
  * (6)
  * agarrar el vector de m y a cada componente la dividis por la norma m
- */
-
-/**
+ *
  * Construction of the normal field, for each pixel
- * (step 5 and 6)
  *
  * @param i1, i2, i3 the matrix with the brightness on each pixel for the 3 images
  * @param s1, s2, s3 the source of light for the images
  * @return the normal field of the image
  */
 matrix<row<double>> normalField(const matrix<double> &i1, const matrix<double> &i2, const matrix<double> &i3,
-                                const direction &s1, const direction &s2, const direction &s3) {
+                                const direction &s1, const direction &s2, const direction &s3, options opts) {
     size_t height = i1.size(), width = i1[0].size();
     PLUMatrix<double> sm = gaussian_elimination(sourceOfLightMatrix(s1, s2, s3));
     matrix<row<double>> normal;
@@ -42,18 +43,31 @@ matrix<row<double>> normalField(const matrix<double> &i1, const matrix<double> &
     for (size_t i = 0; i < height; i++) {
         row<row<double>> r;
         for (size_t j = 0; j < width; j++) {
-            //(5)
-            row<double> m = Matrix::solvePLUSystem(sm.P, sm.L, sm.U, {i1[i][j], i2[i][j], i3[i][j]});
-            //(6)
-            double mNorm = Matrix::twoNorm({m}); //norma de m
-            m[0] /= mNorm;
-            m[1] /= mNorm;
-            m[2] /= mNorm;
-            r.push_back(m);
+            if (opts.maskOptimization && (i1[i][j] == 0 && i2[i][j] == 0 && i3[i][j] == 0)) {
+                r.push_back({0, 0, 0}); //TODO: revisen esto
+            } else {
+                //(5)
+                row<double> m = Matrix::solvePLUSystem(sm.P, sm.L, sm.U, {i1[i][j], i2[i][j], i3[i][j]});
+                //(6)
+                double mNorm = Matrix::twoNorm({m});
+                m[0] /= mNorm;
+                m[1] /= mNorm;
+                m[2] /= mNorm;
+                r.push_back(m);
+            }
         }
         normal.push_back(r);
     }
     return normal;
+}
+
+//Aqui viene lo bueno jovenes, I cho cho choleskyou
+matrix<double> findDepth(const matrix<row<double>> &m) {
+    size_t height = m.size(), width = m[0].size();
+    matrix<double> depth;
+
+    //Aca hay que hacer lo que no se que hacer
+    return depth;
 }
 
 
